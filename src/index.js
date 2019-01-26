6fdb8748e79d6ce2effbdb4b492325ad4cced89e
Vladislav Borisov <vladborisov182@gmail.com>
2019-01-26 20:47:36 +0200

    props validation second lesson

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import CounterButton from './apps/04_props/lesson';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
