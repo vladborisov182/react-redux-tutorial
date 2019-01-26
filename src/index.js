e603e864680df5bf8abc048820b69c50e8b9f1d8
Vladislav Borisov <vladborisov182@gmail.com>
2019-01-26 17:57:17 +0200

    local state

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ButtonCounter from './apps/03_state/lesson';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ButtonCounter />, document.getElementById('root'));
 registerServiceWorker();
