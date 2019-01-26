b949ccebe4f1107d76df1bb637ebbbc3c473ac29
Vladislav Borisov <vladborisov182@gmail.com>
2019-01-26 15:22:11 +0200

    fixed project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
-import Lesson from './apps/lesson_1/components/lesson_1'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import Lesson from './apps/lesson_1/components/lesson_1';
 
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+ReactDOM.render(<Lesson />, document.getElementById('root'));
+registerServiceWorker();
