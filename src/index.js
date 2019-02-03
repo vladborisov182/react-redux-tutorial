bd7ba5ba0780c1fbfb4bd74fb7b513ba963d7503
Vladislav Borisov <vladborisov182@gmail.com>
2019-02-03 16:37:39 +0200

    forms

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Table from './apps/09_fragments/lesson';
+import Form from './apps/10_forms/lesson';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Table />, document.getElementById('root'));
+ReactDOM.render(<Form />, document.getElementById('root'));
 registerServiceWorker();
