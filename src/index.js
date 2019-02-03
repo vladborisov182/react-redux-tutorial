622b39f295a76e8bc460aaaa694ee23602132291
Vladislav Borisov <vladborisov182@gmail.com>
2019-02-03 17:49:58 +0200

    refs start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './apps/10_forms/lesson';
+import App from './apps/12_refs/lesson';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Form />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
