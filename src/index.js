f1cd97146c35bc74dd0b84106edfcb69f6163c20
Vladislav Borisov <vladborisov182@gmail.com>
2019-02-09 11:47:56 +0200

    HOCs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './apps/12_refs/lesson';
+import App from './apps/13_high_order_components/lesson';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Form />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
