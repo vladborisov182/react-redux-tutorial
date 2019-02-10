85b89b95bb757baf1ec588ef2b097d451e8df0c5
Vladislav Borisov <vladborisov182@gmail.com>
2019-02-10 19:08:40 +0200

    portals

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './apps/13_high_order_components/lesson';
+import App from './apps/14_portals/lesson';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
