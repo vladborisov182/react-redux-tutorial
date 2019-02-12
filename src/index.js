2717157f3ffa2671217987f99933f5813c294829
Vladislav Borisov <vladborisov182@gmail.com>
2019-02-12 17:23:14 +0200

    context

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './apps/14_portals/lesson';
+import { App } from './apps/15_context/lesson';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
