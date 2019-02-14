e507cb47b285cecd8b675b7aeb51b1965ce85b18
Vladislav Borisov <vladborisov182@gmail.com>
2019-02-14 22:41:19 +0200

    api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './apps/15_context/lesson';
+import App from './apps/16_work_api/containers/lesson';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
