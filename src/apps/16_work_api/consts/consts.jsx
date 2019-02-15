71533cf46ae4b2cb44bff2bbe4722675f4b0d72f
Vladislav Borisov <vladborisov182@gmail.com>
2019-02-15 16:17:28 +0200

    api 2

--- src/apps/16_work_api/consts/consts.jsx
+++ src/apps/16_work_api/consts/consts.jsx
@@ -1,3 +1,23 @@
 export const BASE_PATH = 'https://hn.algolia.com/api/v1';
 export const SEARCH_PATH = '/search';
 export const SEARCH_PARAM = 'query=';
+export const PAGE_HITS = 'hitsPerPage=';
+export const PAGE_PARAM = 'page=';
+export const HITS = [
+  {
+    value: 10,
+    label: 10,
+  },
+  {
+    value: 20,
+    label: 20,
+  },
+  {
+    value: 30,
+    label: 30,
+  },
+  {
+    value: 40,
+    label: 40,
+  },
+]
--- src/apps/16_work_api/containers/lesson.jsx
+++ src/apps/16_work_api/containers/lesson.jsx
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import Title from '../components/title';
 import NewsPost from '../components/news';
 import Input from '../components/input';
+import Select from '../components/select';
+import Pagination from '../components/paginator';
 
 import { 
   BASE_PATH,
   SEARCH_PATH,
   SEARCH_PARAM,
+  PAGE_HITS,
+  HITS,
+  PAGE_PARAM,
 } from '../consts/consts';
 
 class App extends Component {
@@ -14,30 +19,35 @@ class App extends Component {
   state = {
     searchQuery: '',
     result: {},
+    hitsPerPage: 20,
+    page: 0,
   }
 
   componentDidMount() {
-    const { searchQuery } = this.state;
-    this.fetchData(searchQuery);
+    const { searchQuery, hitsPerPage, page } = this.state;
+    this.fetchData(searchQuery, hitsPerPage, page);
   }
 
-  fetchData = (searchQuery) => {
-    fetch(`${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${searchQuery}`)
+  fetchData = (searchQuery, hitsPerPage, page) => {
+    fetch(`${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${searchQuery}&${PAGE_HITS}${hitsPerPage}&${PAGE_PARAM}${page}`)
       .then(res => res.json())
       .then(result => this.setNews(result))
       .catch(error => error);
   }
 
   handleInputChange = ({ target: { value } }) => {
-    this.setState({ 
-      searchQuery: value,
-    });
+    this.setState({
+      searchQuery: value
+    })
   }
 
   getSearch = ({ key }) => {
     if(key === 'Enter') {
-      const { searchQuery } = this.state;
-      this.fetchData(searchQuery);
+      const { searchQuery, hitsPerPage } = this.state;
+      this.setState({
+        page: 0,
+      })
+      this.fetchData(searchQuery, hitsPerPage, 0);
     }
   }
 
@@ -45,16 +55,60 @@ class App extends Component {
     this.setState({ result });
   }
 
+  handleHitsChange = ({ target: { value } }) => {
+    const { searchQuery } = this.state;
+
+    this.setState({
+      hitsPerPage: +value,
+      page: 0
+    }, () => {
+      this.fetchData(searchQuery, this.state.hitsPerPage, 0);
+    })
+  }
+
+  handlePageChange = ({ target }) => {
+    const btnType = target.getAttribute('data-name');
+    let { page } = this.state;
+
+    if(!isNaN(btnType)) {
+      this.updatePage(+btnType);
+    } else {
+      switch (btnType) {
+        case 'next':
+          this.updatePage(page + 1);
+          break;
+        case 'prev':
+          this.updatePage(page - 1);
+          break;
+      }
+    }
+  }
+
+  updatePage = (number) => {
+    const { searchQuery, hitsPerPage } = this.state;
+    this.setState({
+      page: number,
+    }, () => {
+      this.fetchData(searchQuery, hitsPerPage, number);
+    })
+  }
+
   render() {
-    const { searchQuery, result } = this.state;
-    const { hits = [] } = result;
+    const { searchQuery, result, hitsPerPage } = this.state;
+    const { hits = [], page, nbPages } = result;
 
     return (
       <div className="wrapper">
-        <Title title='Hacker news' />
+        <Title title="Hacker News" />
+        <Select options={HITS} handleChange={this.handleHitsChange} value={hitsPerPage} />
+        <Pagination
+          onClick={this.handlePageChange}
+          page={page}
+          lastPage={nbPages}
+        />
         <Input onKeyPress={this.getSearch} onChange={this.handleInputChange} value={searchQuery} />
         <ul className="newsList">
-          {hits.map(({ author, created_at, num_comments, objectID, title, points, url }) => 
+          {hits.map(({ author, created_at, num_comments, objectID, title, points, url }) =>
             <NewsPost
               key={objectID}
               author={author}
@@ -67,7 +121,7 @@ class App extends Component {
           )}
         </ul>
       </div>
-    )
+    );
   }
 }
 
